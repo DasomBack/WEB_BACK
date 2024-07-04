02b23672b162022b86893ea0e709c3d97be4bc60
alice1the <aliceflo014@1thefull.com>
2024-07-04 14:13:40 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/_thefull/dasom_web_demo/DasomWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com._thefull.dasom_web_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasomWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasomWebDemoApplication.class, args);
+	}
+
+}
