67e2cf16ec0f71b69abafcdcec2193e2dcf82db3
sophie_lavender <wjddbwls@ewhain.net>
2024-07-07 22:54:55 +0900

    feat : 회원가입 화면 구현

--- src/main/java/com/_thefull/dasom_web_demo/controller/UserController.java
+++ src/main/java/com/_thefull/dasom_web_demo/controller/UserController.java
@@ -2,23 +2,41 @@ package com._thefull.dasom_web_demo.controller;
 
 import com._thefull.dasom_web_demo.domain.User;
 import com._thefull.dasom_web_demo.domain.dto.UserJoinRequestDto;
+import com._thefull.dasom_web_demo.service.UserRegisterService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+import javax.swing.text.View;
+
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
-    @PostMapping("/join")
-    public ResponseEntity join(@RequestBody @Valid UserJoinRequestDto dto){
-        System.out.println(dto.getName());
-        return null;
+    private final UserRegisterService userRegisterService;
+
+    @PostMapping("/register")
+    public String register(@RequestParam("store") String store,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("phoneNum") String phoneNum,
+                                 @RequestParam("email") String email,
+                                 @RequestParam("password") String password,
+                                 RedirectAttributes redirectAttributes){
+
+        UserJoinRequestDto dto = new UserJoinRequestDto(store, name, email, phoneNum, password);
+        userRegisterService.registerUser(dto);
+
+        redirectAttributes.addFlashAttribute("name",name);
+
+        return "redirect:/page/user/registersuccess";
     }
+
 }
