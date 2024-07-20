736ab15c4d4fed7cadf633ddccbbd614029a69d8
MoonHaeIn <haein@1thefull.com>
2024-07-20 12:02:42 +0900

    feat : 중복 class 및 사용하지 않는 import 삭제

--- src/main/java/com/_thefull/dasom_web_demo/domain/user/domain/dto/LoginRequestDto.java
+++ src/main/java/com/_thefull/dasom_web_demo/domain/user/domain/dto/LoginRequestDto.java
@@ -1,9 +1,5 @@
 package com._thefull.dasom_web_demo.domain.dto;
 
-import com._thefull.dasom_web_demo.domain.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
