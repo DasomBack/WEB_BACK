262b4d6cbf5932e1092434610a6aa5098bcfd2c8
MoonHaeIn <haein@1thefull.com>
2024-07-21 13:15:24 +0900

    feat : 로그인- 비밀번호 오류 기능 구현

--- src/main/java/com/_thefull/dasom_web_demo/domain/user/controller/LoginController.java
+++ src/main/java/com/_thefull/dasom_web_demo/domain/user/controller/LoginController.java
@@ -5,32 +5,33 @@ import com._thefull.dasom_web_demo.domain.user.domain.User;
 import com._thefull.dasom_web_demo.domain.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class LoginController {
 
-    private final com._thefull.dasom_web_demo.domain.user.service.LoginService LoginService;
+    private final LoginService loginService;
 
     @PostMapping("/login")
     public String login(@RequestParam("store") String store,
                         @RequestParam("phoneNum") String phoneNum,
-                        @RequestParam("password") String password){
+                        @RequestParam("password") String password,
+                        Model model){
 
         LoginRequestDto dto = new LoginRequestDto(store, phoneNum, password);
-        User user = LoginService.login(dto);
-
+        User user = loginService.login(dto);
 
         if (user != null) {
-            return "redirect:/page/user/product";
+            return "pages/user/product";
         } else {
-            return "redirect:/page/user/login";
+            model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
+            return "pages/user/login";
         }
     }
 }
-
-
