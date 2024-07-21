f17bad3a8c57185250ee5ac03c177ad44d9a4079
MoonHaeIn <haein@1thefull.com>
2024-07-21 15:21:15 +0900

    feat : 로그인- 전화번호 또는 비밀번호 불일치시 모달 구현

--- src/main/resources/static/assets/js/login.js
+++ src/main/resources/static/assets/js/login.js
@@ -1,35 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     function validateAndSubmit() {
-        // Get form elements
         const store = document.getElementById('exampleInputStorecode1').value;
         const phoneNum = document.getElementById('exampleInputPhonenum1').value;
         const password = document.getElementById('exampleInputPassword1').value;
 
-        // Check if all fields are filled
         if (!store || !phoneNum || !password) {
             showModal('모든 필드를 올바르게 채워주세요.');
             return;
         }
 
-        // Submit the form if there are no errors
         document.getElementById('loginForm').submit();
     }
 
     function showModal(message) {
-        // Get the modal
         const modal = document.getElementById('errorModal');
 
-        // Change the modal message
         const modalBody = modal.querySelector('.modal-body p');
         if (modalBody) {
             modalBody.textContent = message;
         }
 
-        // Show the modal
         $(modal).modal('show');
     }
 
-    // Check if the close button exists before adding the event listener
     const closeButton = document.querySelector('.modal .close');
     if (closeButton) {
         closeButton.addEventListener('click', function(){
@@ -37,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Check for error message from server
     const errorMessageElement = document.getElementById('errorModal');
     if (errorMessageElement) {
         const errorMessage = errorMessageElement.getAttribute('data-error-message');
@@ -46,6 +38,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Attach validateAndSubmit to the button click event
     document.querySelector('.auth-form-btn').addEventListener('click', validateAndSubmit);
 });
